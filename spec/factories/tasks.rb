69fccef3d89badc527299b25cba0c46fd9dbf2c1
mysthkr4036@gmail.com <hikaru_miyashita>
2023-03-03 02:23:11 +0900

    Create RSpec

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -3,8 +3,20 @@ FactoryBot.define do
     task_name { 'test_name' }
     task_detail { 'test_detail' }
   end
-   factory :second_task, class: Task do
+  factory :second_task, class: Task do
     task_name { 'test_name_2' }
     task_detail { 'test_detail_2' }
+  end
+  factory :third_task, class: Task do
+    task_name { 'test_name_3' }
+    task_detail { 'test_detail_3' }
+  end
+  factory :forth_task, class: Task do
+    task_name { 'test_name_4' }
+    task_detail { 'test_detail_4' }
+  end
+  factory :fifth_task, class: Task do
+    task_name { 'test_name_5' }
+    task_detail { 'test_detail_5' }
   end
 end
