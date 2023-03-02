c4b88f5224869612fa77882badf08190795b03f6
mysthkr4036@gmail.com <hikaru_miyashita>
2023-03-02 16:14:45 +0900

    Create show and edit of function and view, show notice and erro message

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -11,9 +11,26 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     if @task.save
-      redirect_to tasks_path
+      redirect_to tasks_path, notice: "タスクを作成しました！"
     else
       render :new
+    end
+  end
+  
+  def show
+    @task = Task.find(params[:id])
+  end
+  
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to tasks_path, notice: "タスクを編集しました！"
+    else
+        render :edit
     end
   end
   
