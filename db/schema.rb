f9a82815888937d972624c8433288bc18f73e4fd
mysthkr4036@gmail.com <hikaru_miyashita>
2023-03-03 14:57:48 +0900

    DB migrated of DueDate and see duedate in index. See duedate in new page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_02_054949) do
+ActiveRecord::Schema.define(version: 2023_03_03_053625) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2023_03_02_054949) do
     t.string "task_detail", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.datetime "due_date", default: "2023-03-02 15:21:26", null: false
   end
 
 end
