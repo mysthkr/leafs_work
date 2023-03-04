aaa02207d0c267f050c8e45952fca06758ca3174
mysthkr4036@gmail.com <hikaru_miyashita>
2023-03-05 01:59:45 +0900

    Add User by seed and manual

--- db/schema.rb
+++ db/schema.rb
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 2023_03_04_160704) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "digest_password"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
