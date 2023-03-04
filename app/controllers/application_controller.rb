c59e7fde6a7cac260fcb0d420582c45f1690e05a
mysthkr4036@gmail.com <hikaru_miyashita>
2023-03-05 03:23:13 +0900

    Add user and session function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,16 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :login_required
   # before_action :basic_auth 
+  include SessionsHelper
+  
 
-  # private
+  private
 
+  def login_required
+    redirect_to new_session_path unless current_user
+  end
+  
   # def basic_auth
   #   authenticate_or_request_with_http_basic do |username, password|
   #     username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_04_160704) do
+ActiveRecord::Schema.define(version: 2023_03_04_172027) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,9 @@ ActiveRecord::Schema.define(version: 2023_03_04_160704) do
     t.datetime "due_date", default: "2023-03-02 15:21:26", null: false
     t.string "status", default: "ToDo", null: false
     t.integer "priority"
+    t.bigint "user_id", null: false
     t.index ["task_name"], name: "index_tasks_on_task_name"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -33,6 +35,8 @@ ActiveRecord::Schema.define(version: 2023_03_04_160704) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["id"], name: "index_users_on_id"
   end
 
+  add_foreign_key "tasks", "users"
 end
